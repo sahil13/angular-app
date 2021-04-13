d2c5e8a40d9707121ed42280f482bc12494b8de7
sahil13 <sahil834@gmail.com>
2021-04-13 12:01:58 +0530

    post component dynamic columns and sorting

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PostsService } from '../common/posts.service';
 
-
-
-
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -19,6 +16,14 @@ export class PostComponent implements OnInit {
   postForm = new FormGroup({
     title: new FormControl('', [Validators.required])
   });
+
+  cols = [
+    { field: 'id', header: 'ID', width: '70', sort: true },
+    { field: 'title', header: 'Title', width: '120' },
+    { field: 'body', header: 'Body', width: '120' },
+    { field: 'like', header: 'Like', width: '80', sort: true },
+    { field: 'action', header: 'Action', width: '90', sort: true }
+  ];
 
   constructor(private postsService: PostsService) {}
 
@@ -38,12 +43,14 @@ export class PostComponent implements OnInit {
   searchRecord(postForm) {
     const title = postForm.controls.title.value;
     if (title) {
-      this.filteredPost = this.posts.filter(post => post.title.includes(title.toLowerCase()));
+      this.filteredPost = this.posts.filter(post =>
+        post.title.includes(title.toLowerCase())
+      );
     } else {
       this.filteredPost = this.posts;
     }
   }
-  clicked(item){
+  clicked(item) {
     // alert(item);
   }
 }
